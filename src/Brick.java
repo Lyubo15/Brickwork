257aac5b0e9550fd36e438249a3f2bd69ef15414
Lubomir <60439657+Bubo15@users.noreply.github.com>
2021-01-22 17:11:22 +0200

    Update Brick.java

--- src/Brick.java
+++ src/Brick.java
@@ -1,20 +1,23 @@
 public class Brick {
 
     // set brick in layer
-    public void setBrickInLayout(int[][] layer, int firstPartRow, int firstPartCol, int secondPartRow, int secondPartCol, int k) {
+    public void setBrickInLayer(int[][] layer, int firstPartRow, int firstPartCol, int secondPartRow, int secondPartCol, int k) {
         layer[firstPartRow][firstPartCol] = k;
         layer[secondPartRow][secondPartCol] = k;
     }
 
+    // check, if given brick is in bound
     public boolean isInBound(int start, int end, int row, int col) {
         return end < col && end >= 0 && start >= 0 && start < row;
     }
 
+    // chack, if have empty space by given coordinates
     public boolean hasEmptyPlace(int[][] layer, int firstPartStart, int firstPartEnd, int secondPartStart, int secondPartEnd) {
         return layer[firstPartStart][firstPartEnd] == 0 && layer[secondPartStart][secondPartEnd] == 0;
     }
 
-    public boolean isOverLayoutOne(int[][] layer, int firstPartStart, int firstPartEnd, int secondPartStart, int secondPartEnd) {
+    // chack, if brick from second layer is over first layer
+    public boolean isOverLayerOne(int[][] layer, int firstPartStart, int firstPartEnd, int secondPartStart, int secondPartEnd) {
         return layer[firstPartStart][firstPartEnd] != layer[secondPartStart][secondPartEnd];
     }
 }
